eed7a2928912af8fbb35f9955ec1374c740c6b05
BenmradBilel <benmradbilel@ubuntu20.myguest.virtualbox.org>
2023-11-26 19:34:46 +0000

    Final version of dockerizing the mern app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,52 @@
-import { useEffect, useState } from 'react';
-import { Users } from './components';
-import UserDetailsScreen from './screens/UserDetailsScreen';
-import Axios from 'axios';
+import { useEffect, useState } from "react";
+import { Users } from "./components";
+import UserDetailsScreen from "./screens/UserDetailsScreen";
+import Axios from "axios";
+import { Button } from "./components/ui/button";
 
-const BASE_URL = `http://backend:5000/api/users`;
+const BASE_URL = `http://localhost:5000/api/users`;
 
 const App = () => {
-	const [users, setUsers] = useState([]);
-	const [selectedUser, setSelectedUser] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
 
-	const onSetSelectedUserHandler = (user) => setSelectedUser(user);
+  const onSetSelectedUserHandler = (user) => setSelectedUser(user);
+  const onAddUserHandler = async () => {
+    try {
+      const user = {
+        userName: "Benmrad Bilel",
+        email: `bilel${Date.now()}@gmail.com`,
+        password: "1234560",
+      };
 
-	useEffect(() => {
-		const fetchUsers = async () => {
-			const {
-				data: { data },
-			} = await Axios.get(BASE_URL);
-			setUsers(data);
-		};
+      await Axios.post(`${BASE_URL}/new`, user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-		fetchUsers();
-	}, [users]);
-	return (
-		<main className='w-full flex justify-center gap-3 p-4'>
-			<Users users={users} onClickUser={onSetSelectedUserHandler} />
-			{selectedUser && <UserDetailsScreen user={selectedUser} />}
-		</main>
-	);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {
+        data: { data },
+      } = await Axios.get(BASE_URL);
+      console.log(`Data : ${data}`);
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, [users]);
+  return (
+    <>
+      <Button className="m-4" onClick={onAddUserHandler}>
+        Add New User
+      </Button>
+      <main className="w-full flex justify-center gap-3 p-4">
+        <Users users={users} onClickUser={onSetSelectedUserHandler} />
+        {selectedUser && <UserDetailsScreen user={selectedUser} />}
+      </main>
+    </>
+  );
 };
 
 export default App;
